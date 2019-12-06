30f21092e7ab9e93002021c73d680d7813eb4a9c
LAPTOP-RN7KLM21\Syeda Tayyab Ali <ssaeed@folio3.com>
2019-12-06 23:22:34 +0500

    add registering in the project and route of registering in the index.js file

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const rentals = require('./routes/rentals');
 const movies = require('./routes/movies');
+const users = require('./routes/users');
 
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port${port}...`));
