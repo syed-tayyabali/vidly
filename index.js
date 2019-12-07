d4c97357e5797c19f366fe749bac2f41bce66a87
LAPTOP-RN7KLM21\Syeda Tayyab Ali <ssaeed@folio3.com>
2019-12-07 22:39:26 +0500

    authenticating a user

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const customers = require('./routes/customers');
 const rentals = require('./routes/rentals');
 const movies = require('./routes/movies');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 
 app.use(express.json());
 app.use('/api/genres', genres);
@@ -20,6 +21,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port${port}...`));
