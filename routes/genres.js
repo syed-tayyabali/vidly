038a376710c36d3b40a2ef46d434487d5518c821
LAPTOP-RN7KLM21\Syeda Tayyab Ali <ssaeed@folio3.com>
2019-12-11 16:29:58 +0500

    add admin and perform delete functionality to check

--- routes/genres.js
+++ routes/genres.js
@@ -1,4 +1,5 @@
 const auth = require('../middleware/auth');
+const admin = require('../middleware/admin');
 const { Genre, validateGenre } = require('../models/genre');
 const mongoose = require('mongoose');
 const express = require('express');
@@ -39,7 +40,7 @@ router.put('/:id', auth, async (req, res) => {
     res.send(genre);
 });
 
-router.delete('/:id', auth, async (req, res) => {
+router.delete('/:id', [auth, admin], async (req, res) => {
     const genre = await Genre.findByIdAndRemove(req.params.id);
     if (!genre) {
         return res.status(404).send('the genre with the given id is not available');
