94dddff6d359390cdbb9ba65db3af237c8d81f68
LAPTOP-RN7KLM21\Syeda Tayyab Ali <ssaeed@folio3.com>
2019-12-11 16:13:50 +0500

    implemet user information to the login user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
+const auth = require('../middleware/auth');
 const { User, validateUser } = require('../models/user');
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
+
+router.get('/me', auth, async (req, res) => {
+    const user = await User.findById(req.user._id)
+        .select('-password');
+    res.send(user);
+});
 
 router.post('/', async (req, res) => {
     const { error } = validateUser(req.body);
