2a971c1d0dc8c737e0be37469fe9a59137d5560a
LAPTOP-RN7KLM21\Syeda Tayyab Ali <ssaeed@folio3.com>
2019-12-14 16:42:36 +0500

    handled promise rejection in index.js and store it in the logfile

--- index.js
+++ index.js
@@ -20,13 +20,24 @@ const movies = require('./routes/movies');
 const users = require('./routes/users');
 const auth = require('./routes/auth');
 
+process.on('uncaughtException', (ex) => {
+    winston.error(ex.message, ex);
+    process.exit(1);
+});
+
+process.on('unhandledRejection', (ex) => {
+    winston.error(ex.message, ex);
+    process.exit(1);
+});
+
 winston.add(new winston.transports.File({ filename: 'logfile.log' }));
 winston.add(new winston.transports.MongoDB({
     db: 'mongodb://localhost/vidly',
     level: 'error'
 }));
 
-throw new Error('something failed during startup');
+const p = Promise.reject(new Error('rejected promise'));
+p.then(() => console.log('done'));
 
 if (!config.get('jwtPrivateKey')) {
     console.error('FATAL ERROR: jwtPrivateKey is not defined');
